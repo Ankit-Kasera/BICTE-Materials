3ab67ea6ac2e753e843d4e176ad1696b2c7c3d1e
Ankit <ankitkasera2076@gmail.com>
2026-02-15 23:45:58 +0545

    changed logo

--- App.tsx
+++ App.tsx
@@ -80,7 +80,7 @@ const App: React.FC = () => {
             <div className="w-10 h-10 bg-blue-600 rounded-lg flex items-center justify-center shadow-lg transition-transform group-hover:rotate-6 group-hover:scale-110">
               <span className="text-white font-black text-xl italic">B</span>
             </div>
-            <h1 className="text-2xl font-black tracking-tight text-slate-800 dark:text-slate-100">BICTE</h1>
+            <h1 className="text-2xl font-black tracking-tight text-slate-800 dark:text-slate-100">BICTE Muji</h1>
           </button>
           
           <div className="flex items-center space-x-4">
